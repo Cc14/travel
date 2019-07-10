d839ba2ad8025920efef94a5dd370fc03fdba40f
Cc14 <313659961@qq.com>
2019-07-10 14:39:03 +0800

    keep-alive

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -9,43 +9,56 @@
 </template>
 
 <script>
-    import Header from './components/Header';
-    import HomeSwiper from './components/Swiper'
-    import Icons from './components/Icons'
-    import Recommend from './components/Recommend'
-    import Weekend from './components/Weekend'
-    import axios from 'axios'
-    export default {
-        data() {
-            return {
-                iconList: [],
-                recommendList:[],
-                swiperList:[],
-                weekendList:[]
-            }
-        },
-        components:{
-            Header, HomeSwiper, Icons, Recommend, Weekend
-        },
-        mounted() {
-            this.getHomeInfo()
-        },
-        methods: {
-            getHomeInfo(){
-                axios.get('/api/index.json')
-                    .then((res)=>{
-                        let data = res.data.data
-                        this.iconList = data.iconList
-                        this.recommendList = data.recommendList
-                        this.swiperList = data.swiperList
-                        this.weekendList = data.weekendList
-
-                    })
-            }
-        }
+import Header from "./components/Header";
+import HomeSwiper from "./components/Swiper";
+import Icons from "./components/Icons";
+import Recommend from "./components/Recommend";
+import Weekend from "./components/Weekend";
+import axios from "axios";
+import { mapState } from "vuex";
+export default {
+  data() {
+    return {
+      iconList: [],
+      recommendList: [],
+      swiperList: [],
+      weekendList: [],
+      lastCity: ""
+    };
+  },
+  components: {
+    Header,
+    HomeSwiper,
+    Icons,
+    Recommend,
+    Weekend
+  },
+  methods: {
+    getHomeInfo() {
+      axios.get("/api/index.json?city=" + this.city).then(res => {
+        let data = res.data.data;
+        this.iconList = data.iconList;
+        this.recommendList = data.recommendList;
+        this.swiperList = data.swiperList;
+        this.weekendList = data.weekendList;
+      });
     }
+  },
+  computed: {
+    ...mapState(["city"])
+  },
+  mounted() {
+    this.lastCity = this.city;
+    this.getHomeInfo();
+  },
+  activated() {
+    if(this.lastCity !== this.city){
+        this.lastCity = this.city
+        this.getHomeInfo()
+    }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
